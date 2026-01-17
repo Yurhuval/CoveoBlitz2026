50b256f9d74e946747dd823f9e4140bc7e18bc76
Yurhuval <Yatre47@ulaval.ca>
2026-01-17 11:49:47 -0500

    Added multiples strats

---
+++ SpawnerBlitzStrategy.py
@@ -0,0 +1,28 @@
+import math
+
+import Utils
+from SporeStrategy import SporeStrategy
+from game_message import Spore, TeamGameState, Spawner, Position, SporeMoveToAction
+
+
+class SpawnerBlitzStrategy(SporeStrategy):
+    def __init__(self, destination: Position = None):
+        super().__init__()
+        self.destination : Position = None
+    def get_action(self, spore: Spore, game_message: TeamGameState):
+        if self.destination is None or spore.position == self.destination:
+            self.destination = self.find_destination(spore,game_message)
+        return SporeMoveToAction(spore.id,self.destination)
+
+    def find_destination(self, spore: Spore, game_message: TeamGameState) -> Position:
+        team_info = game_message.world.teamInfos[game_message.yourTeamId]
+        max_dist = math.inf
+        destination = None
+        for spawner in [spawner for spawner in game_message.world.spawners if spawner not in team_info.spawners]:
+            dist = Utils.get_distance(spore.position, spawner.position)
+            if dist < max_dist and dist > 0  :
+                max_dist = dist
+                destination = spawner.position
+        return destination
+
+
