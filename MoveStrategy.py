4c0f7d804ad739dd3f4ed64ce80feb7363a5fa13
Yurhuval <Yatre47@ulaval.ca>
2026-01-17 12:21:13 -0500

    added blitz strategies

--- MoveStrategy.py
+++ MoveStrategy.py
@@ -4,7 +4,9 @@ from game_message import Position, Spore, SporeMoveToAction, TeamGameState
 
 class MoveStrategy(SporeStrategy):
     def __init__(self,destination: Position):
+        super().__init__()
         self.destination = destination
+
 
     def get_action(self, spore: Spore, game_message: TeamGameState):
         return SporeMoveToAction(spore.id,self.destination)
